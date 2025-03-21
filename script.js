52fa9573d109d39d4999ec0e12eccb9c6ad70ee9
shannkz04 <kaitoniisan2000@gmail.com>
2025-03-20 21:49:22 -0600

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("¡Bienvenido a nuestra web de proyectos!");
 });
+function mostrarPDF(pdfUrl) {
+    // Cambia el src del iframe para mostrar el PDF
+    document.getElementById('pdfViewer').src = pdfUrl;
+}
