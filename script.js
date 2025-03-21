6828f97bbc8082a01da67953c5a6528d3b4f4245
shannkz04 <kaitoniisan2000@gmail.com>
2025-03-21 02:23:02 -0600

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("¡Bienvenido a Kusomagi! Únete a la aventura.");
     
-document.addEventListener("DOMContentLoaded", function() {
     const shareButton = document.getElementById("share-button");
     const shareMenu = document.getElementById("share-menu");
     const body = document.body;
+
     // URL de la página que se quiere compartir
     const url = "https://shannkz04.github.io/el-ojo-de-shannkz";
     const image = "https://shannkz04.github.io/el-ojo-de-shannkz/prueba.png"; // Imagen para compartir
