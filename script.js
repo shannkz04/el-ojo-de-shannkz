2fb085f550e9baf416e61919fbb335718d54565c
shannkz04 <kaitoniisan2000@gmail.com>
2025-03-20 22:34:06 -0600

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
 function mostrarPDF() {
     let container = document.getElementById('visor-pdf-container');
 
-    // Si el visor ya está creado, no lo duplicamos
+    // Si ya existe el visor, no lo duplicamos
     if (!document.getElementById('visor-pdf')) {
         let iframe = document.createElement('iframe');
         iframe.id = 'visor-pdf';
-        iframe.src = "https://raw.githubusercontent.com/shannkz04/el-ojo-de-shannkz/994fec971b406157c7bc21a8b3a1ee42d22f801a/KusoMagi%20TOMO%201.pdf";
+        iframe.src = "https://docs.google.com/gview?url=https://raw.githubusercontent.com/shannkz04/el-ojo-de-shannkz/994fec971b406157c7bc21a8b3a1ee42d22f801a/KusoMagi%20TOMO%201.pdf&embedded=true";
         iframe.style.width = "100%";
         iframe.style.height = "600px";
         iframe.style.border = "none";
