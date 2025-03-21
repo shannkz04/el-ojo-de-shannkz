58fd69ac9fa1bf0264b0605b65ac51e2ca78538b
shannkz04 <kaitoniisan2000@gmail.com>
2025-03-20 22:13:21 -0600

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("¡Bienvenido a nuestra web de proyectos!");
 });
-function mostrarPDF(ruta) {
+function mostrarPDF() {
     let visor = document.getElementById('visor-pdf');
 
     if (!visor) {
-        // Si no existe el visor, lo creamos
         visor = document.createElement('iframe');
         visor.id = 'visor-pdf';
         visor.style.width = '100%';
@@ -14,6 +13,6 @@ function mostrarPDF(ruta) {
         document.querySelector('.descargas').appendChild(visor);
     }
 
-    // Mostrar el PDF en el visor
-    visor.src = ruta;
+    visor.src = "https://docs.google.com/viewer?url=" + 
+        encodeURIComponent("https://raw.githubusercontent.com/shannkz04/el-ojo-de-shannkz/994fec971b406157c7bc21a8b3a1ee42d22f801a/KusoMagi%20TOMO%201.pdf") + "&embedded=true";
 }
