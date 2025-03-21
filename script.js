09f8c09f8b8e62b04bc2996d13169789a6177744
shannkz04 <kaitoniisan2000@gmail.com>
2025-03-20 22:44:28 -0600

    script.js

--- script.js
+++ script.js
@@ -3,17 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function mostrarPDF() {
-    let container = document.getElementById('visor-pdf-container');
-
-    // Si ya existe el visor, no lo duplicamos
-    if (!document.getElementById('visor-pdf')) {
-        let iframe = document.createElement('iframe');
-        iframe.id = 'visor-pdf';
-        iframe.src = "https://docs.google.com/gview?url=https://raw.githubusercontent.com/shannkz04/el-ojo-de-shannkz/KusoMagi TOMO 1.pdf";
-        iframe.style.width = "100%";
-        iframe.style.height = "600px";
-        iframe.style.border = "none";
-
-        container.appendChild(iframe);
-    }
+    let contenedor = document.getElementById("visor-pdf-container");
+    contenedor.innerHTML = `
+        <iframe src="https://docs.google.com/gview?url=https://raw.githubusercontent.com/shannkz04/el-ojo-de-shannkz/994fec971b406157c7bc21a8b3a1ee42d22f801a/KusoMagi%20TOMO%201.pdf&embedded=true"
+        style="width:100%; height:600px;" frameborder="0"></iframe>
+    `;
 }
