5828a080194784d8a25080b7ea9b4eb8ddee9367
shannkz04 <kaitoniisan2000@gmail.com>
2025-03-21 00:17:30 -0600

    script.js

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ document.getElementById('gifButton').addEventListener('click', function() {
     const gifContainer = document.getElementById('gifContainer');
     gifContainer.innerHTML = `<img src="${randomGif}" alt="Gif Aleatorio" class="gif-random">`;
 });
+document.getElementById('commentForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar el envío del formulario y la recarga de la página
+
+    const commentText = document.getElementById('commentText').value;
+    if (commentText.trim() !== '') {
+        const commentContainer = document.getElementById('commentsContainer');
+        const newComment = document.createElement('div');
+        newComment.classList.add('comment');
+        newComment.innerHTML = `<p>${commentText}</p>`;
+        commentContainer.appendChild(newComment);
+
+        // Limpiar el textarea después de enviar el comentario
+        document.getElementById('commentText').value = '';
+    }
+});
